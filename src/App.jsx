b455749a096b72fc2d50322100f4bd536ea7d804
ay <shlemafona@gmail.com>
2022-02-01 23:36:12 +0200

    prittier enabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-
 import './App.css';
 import Navbar from './app/components/ui/navbar';
 
 function App() {
-  return (
-    <div className='container-xxl'>
-      <Navbar/>
-      <h1>Super application</h1>
-    </div>
-  );
+	return (
+		<div className='container-xxl'>
+			<Navbar />
+			<h1>Super application</h1>
+		</div>
+	);
 }
 
 export default App;
