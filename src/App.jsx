440e4153d1a23b009fbf65b714f1a37ee23ce2fb
ay <shlemafona@gmail.com>
2022-02-01 23:15:04 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './app/components/ui/navbar';
+
+function App() {
+  return (
+    <div className='container-xxl'>
+      <Navbar/>
+      <h1>Super application</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/components/ui/navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <div className="navbar">
+                    <span class="navbar-brand">
+                        Navbar text with an inline element
+                    </span>
+                </div>
+                <div className="navbar">
+                    <span class="navbar-text">
+                        login
+                    </span>
+                </div>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;<h1>Navbar component</h1>
