9628c45d922825fed2f1f23929ee870fe15f352d
ay <shelmafona@gmail.com>
2022-02-02 10:13:58 +0200

    Add toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
+
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Navbar from './app/components/ui/navbar';
 
 function App() {
+	useEffect(() => {
+		toast('App started');
+	}, []);
+
 	return (
 		<div className='container-xxl'>
 			<Navbar />
 			<h1>Super application</h1>
+			<ToastContainer />
 		</div>
 	);
 }
