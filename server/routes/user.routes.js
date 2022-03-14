be8754dacfe4e3e198ff156d0976f172bd6a4a72
ay <shlemafona@gmail.com>
2022-03-14 11:01:53 +0200

    server/refactor: code cleaned

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -10,7 +10,6 @@ router.get('/', useAuthMiddleware(), async (req, res) => {
 	try {
 		const userData = (await userService.findById(userId))._doc;
 		delete userData.password;
-		console.log(userData);
 		res.status(200).json(userData);
 	} catch (err) {
 		const errResponse = new ErrorResponse(500, 'DB_ERROR: ' + err?.message || err?.toString());
