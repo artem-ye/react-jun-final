562d2f573eaa690dfddfe049e8ec90922758dca4
ay <shlemafona@gmail.com>
2022-03-25 13:04:12 +0200

    productsReducer/try-catch added

--- client/src/app/store/reducers/products.reducer.js
+++ client/src/app/store/reducers/products.reducer.js
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 import productsService from '../../services/products.api.service';
-// import productsCategoriesService from '../../services/productsCategories.api.service';
 
 const INITIAL_STATE = {
 	isLoaded: false,
@@ -35,41 +34,12 @@ const { reducer: productsReducer, actions } = productsCategoriesSlice;
 const loadProducts = () => async (dispatch) => {
 	dispatch(actions.loadProductsRequested());
 
-	const products = await productsService.getProducts();
-
-	// const products = [
-	// 	{
-	// 		_id: '62013eb6953086c62b896afa',
-	// 		sku: '97903',
-	// 		title: 'Кольцо',
-	// 		image: 'http://img.nothingshop.com/images/97903/preview.jpg',
-	// 		price: 3,
-	// 		category: '62013b9b272189ab7438d14d',
-	// 		createdAt: '2022-02-07T15:45:58.746Z',
-	// 		updatedAt: '2022-02-07T15:45:58.746Z',
-	// 		__v: 0,
-	// 	},
-	// 	{
-	// 		_id: '62013eb6953086c62b896afb',
-	// 		sku: '97872',
-	// 		title: 'Кольцо',
-	// 		image: 'http://img.nothingshop.com/images/97872/preview.jpg',
-	// 		price: 2,
-	// 		category: '62013b9b272189ab7438d14d',
-	// 		createdAt: '2022-02-07T15:45:58.746Z',
-	// 		updatedAt: '2022-02-07T15:45:58.746Z',
-	// 		__v: 0,
-	// 	},
-	// ];
-	dispatch(actions.loadProductsSuccess(products));
-
-	// try {
-	// 	const categories = await productsCategoriesService.getCategories();
-	// 	const sotredCategories = categories.sort((a, b) => (a.title > b.title ? 1 : -1));
-	// 	dispatch(actions.loadProductsCategoriesSuccess(sotredCategories));
-	// } catch (err) {
-	// 	dispatch(actions.loadProductsCategoriesFailed(err));
-	// }
+	try {
+		const products = await productsService.getProducts();
+		dispatch(actions.loadProductsSuccess(products));
+	} catch (err) {
+		dispatch(actions.loadProductsFailed(err));
+	}
 };
 
 const getProducts = () => (state) => state.products.entities;
